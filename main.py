0342963a88019fbe4a49e964218a9b886b7b3028
liambloom <liamrbloom@gmail.com>
2025-05-07 16:31:25 -0400

    Tested + fixed relative individual info
    Added test sequences and replaced pseudo counts with pseudo frequencies

--- main.py
+++ main.py
@@ -11,27 +11,31 @@ import math
 # If you pass in background nucleotide frequencies, it calculates the weigh matrix
 #   to be used for relative individual information. Otherwise, it calculates the weigh
 #   matrix for regular individual information
-def weigh_matrix(sequences, start, end, background = {"A": 0.25, "G": 0.25, "T": 0.25, "C": 0.25}):
+DEFAULT_BACKGROUND = {"A": 0.25, "G": 0.25, "T": 0.25, "C": 0.25}
+
+test_sequences = ['ACGTACGA', 'ACGTACGT', 'ACGTCCGT', 'ACGTCCAT', 
+                  'ACAGGCAT', 'ACAGGCTT', 'ACAGTCTT', 'ACAGTCTT']
+
+def weigh_matrix(sequences, start, end, background = DEFAULT_BACKGROUND):
     nucleotide_table = []
     for _ in range(start, end):
-        nucleotide_table += [{"A": 1, "G": 1, "T": 1, "C": 1}]
+        nucleotide_table += [{"A": 0, "G": 0, "T": 0, "C": 0}]
     for seq in sequences:
         for i in range(start, end):
             nucleotide_table[i - start][seq[i]] += 1
     result = []
     for i in range(0, end - start):
-        result[i] = {}
+        result += [{}]
         for n in ['A', "G", "T", "C"]:
-            result[i][n] = math.log2(nucleotide_table[i][n] / len(sequences) / background[n])
+            result[i][n] = math.log2((nucleotide_table[i][n] + background[n]) / (len(sequences) + 1) / background[n])
     return result
 
 
-def relative_individual_information(sequences, start, end, background = {"A": 0.25, "G": 0.25, "T": 0.25, "C": 0.25}):
+def relative_individual_information(target, sequences, start, end, background = DEFAULT_BACKGROUND):
     wm = weigh_matrix(sequences, start, end, background)
-    result = []
-    for seq in sequences:
-        rii = 0
-        for i in range(0, end - start):
-            rii += wm[i][seq[i + start]]
-        result += [rii]
+    result = 0
+    for i in range(0, end-start):
+        result += wm[i][target[i + start]]
     return result
+
+print(relative_individual_information('ACGTACGA', test_sequences, 0, 8))
